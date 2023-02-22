db6a219796e0144f25f6d0afd4665182558f52cd
Brae Webb <email@braewebb.com>
2023-02-22 01:17:08 +0000

    Health endpoint

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask 
- 
-def create_app(): 
-    app = Flask(__name__) 
+from flask import Flask
 
-    from .views.routes import api 
-    app.register_blueprint(api) 
+def create_app():
+    app = Flask(__name__)
+
+    from .views.routes import api
+    app.register_blueprint(api)
 
     return app
- 
--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,39 +1,7 @@
-from flask import Blueprint, jsonify
- 
-api = Blueprint('api', __name__, url_prefix='/api/v1') 
+from flask import Blueprint
 
-TEST_ITEM = {
-"id": 1,
-"title": "Watch CSSE6400 Lecture",
-"description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-"completed": True,
-"deadline_at": "2023-02-27T00:00:00",
-"created_at": "2023-02-20T00:00:00",
-"updated_at": "2023-02-20T00:00:00"
-}
- 
-@api.route('/health') 
-def health(): 
-   return jsonify({"status": "ok"})
+api = Blueprint('api', __name__, url_prefix="/api/v1")
 
-
-@api.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify([TEST_ITEM])
-
-@api.route('/todos/<int:todo_id>', methods=['GET'])
-def get_todo(todo_id):
-    return jsonify(TEST_ITEM)
-
-@api.route('/todos', methods=['POST'])
-def create_todo():
-    return jsonify(TEST_ITEM), 201
-
-@api.route('/todos/<int:todo_id>', methods=['PUT'])
-def update_todo(todo_id):
-    return jsonify(TEST_ITEM)
-
-@api.route('/todos/<int:todo_id>', methods=['DELETE'])
-def delete_todo(todo_id):
-    return jsonify(TEST_ITEM)
- 
+@api.route("/health")
+def health():
+    return "ok"
